351f6756eb83a90f11647d46038f08c95eea6ea5
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-01 17:46:25 +0600

    add submit btn

--- src/pages/practice.tsx
+++ src/pages/practice.tsx
@@ -1,7 +1,8 @@
 import { ChangeEvent, useState } from "react"
 import { qna } from "../lib/qna";
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { renderAnswer } from "../lib/renderAnswer";
+import NotFoundCard from "../components/not-found-card";
 
 export default function Practice() {
     const [char, setChar] = useState('');
@@ -17,29 +18,28 @@ export default function Practice() {
         setChar(e.target.value)
     }
 
-    if (context === undefined) {
-        return <div className="h-screen flex items-center justify-center">
-            <div className="flex flex-col items-center justify-center p-12 rounded border border-blue-700 border-dashed">
-                <p className="font-bold text-4xl bg-gradient-to-br from-blue-600 to-blue-900 bg-clip-text text-transparent">404</p>
-                <p className="bg-gradient-to-br from-blue-600 to-blue-900 bg-clip-text text-transparent">Question not found</p>
+    const handleSubmit = (e:ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        // console.log(char)
+    }
 
-                <div className="flex items-center gap-2 mt-4">
-                    <Link className="bg-gradient-to-br from-blue-600 to-blue-900 p-2 rounded text-white flex items-center justify-center hover:to-blue-950 active:ring cursor-pointer select-none" to={'/'}>Home</Link>
-                    <div className="bg-gradient-to-br from-blue-600 to-blue-900 p-2 rounded text-white flex items-center justify-center hover:to-blue-950 active:ring cursor-pointer select-none" onClick={() => { window.history.back() }}>Back</div>
-                </div>
-            </div>
-        </div>
+    if (context === undefined) {
+        return <NotFoundCard content='Question' />
     }
 
     return (
         <div className="p-12 border rounded">
             <div>
-                <div >{context.question}</div>
-                <div className="select-none font-bold md:text-2xl text-xl border flex-wrap flex p-2">
+                <div>{context.question}</div>
+                <div className="select-none font-bold md:text-2xl text-xl border flex-wrap flex p-2 g-text">
                     {renderAnswer(context.answer, char)}
                 </div>
 
-                <textarea onChange={handlePromptChange} rows={6} placeholder="Enter the answer below" className="p-2 mt-4 w-full rounded border outline-none focus:ring ring-blue-200 focus:border-none" />
+                <form onSubmit={handleSubmit}>
+                    <textarea onChange={handlePromptChange} rows={6} placeholder="Enter the answer below" className="p-2 mt-4 w-full rounded border outline-none focus:ring ring-blue-200 focus:border-none" />
+
+                    <button className="mt-2 btn w-full" type="submit">Submit</button>
+                </form>
             </div>
 
         </div>
