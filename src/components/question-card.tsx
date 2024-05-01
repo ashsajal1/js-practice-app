0329de93739344986aabdb329a01e07f747a7eeb
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-01 12:51:35 +0600

    Update question-card.tsx

--- src/components/question-card.tsx
+++ src/components/question-card.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function QuestionCard({ question, answer, id }: { question: string, answer: string, id: number }) {
     return (
-        <div className="flex flex-col rounded p-4 border w-[300px] ">
+        <div className="flex flex-col rounded p-4 border w-full md:w-[300px]">
             <p className='font-bold'>{question}</p>
             <p className='font-light'>{answer}</p>
             <div className='mt-4'>
