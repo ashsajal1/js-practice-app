a45cff020246cf2be1cbb2fc53a6537247ca04db
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-01 10:58:44 +0600

    show specific question

---
+++ src/lib/qna.tsx
@@ -0,0 +1,18 @@
+type QnaTypes = { id: number, question: string, answer: string }
+export const qna: QnaTypes[] = [
+  {
+    id: 1,
+    question: "What is promise in Javascript?",
+    answer: "A promise in JavaScript represents the eventual completion or failure of an asynchronous operation, encapsulating its result."
+  },
+  {
+    id: 2,
+    question: "What is promise in Javascript?",
+    answer: "A promise in JavaScript represents the eventual completion or failure of an asynchronous operation, encapsulating its result."
+  },
+  {
+    id: 3,
+    question: "What is promise in Javascript?",
+    answer: "A promise in JavaScript represents the eventual completion or failure of an asynchronous operation, encapsulating its result."
+  },
+]
