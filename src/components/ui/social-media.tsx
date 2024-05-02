c23219d8f3cb3c9758cad3386447c9f80d8f5eb5
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-02 23:23:13 +0600

    add social share ink

--- src/components/ui/social-media.tsx
+++ src/components/ui/social-media.tsx
@@ -1,7 +1,17 @@
+import { CiTwitter, CiLinkedin, CiFacebook, CiLink } from "react-icons/ci";
+
+const text = `I've practice an interview question in JsPracticeWeb with fun. You can also try https://js-practice-app.vercel.app. #js_practice_app #javascript #interview_practice`
+
 export default function SocialMedia() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h2 className="m-2 dark:text-white">Share to social media</h2>
+            <div className="flex items-center gap-2">
+                <a title="Link to share in social media" href={`https://twitter.com/intent/tweet?text=${text}`} className="btn"><CiTwitter className="h-6 w-6" /></a>
+                <a href={`https://www.linkedin.com/shareArticle?mini=true&url=https://js-practice-app.vercel.app&title=${text}`} title="Link to share in social media" className="btn"><CiLinkedin className="h-6 w-6" /></a>
+                <a href={`https://www.facebook.com/sharer.php?u=https://https://js-practice-app.vercel.app`} title="Link to share in social media" className="btn"><CiFacebook className="h-6 w-6" /></a>
+                <div onClick={() => window.navigator.clipboard.writeText('https://js-practice-app.vercel.app')} className="btn"><CiLink className="h-6 w-6" /></div>
+            </div>
+        </div>
+    )
 }
