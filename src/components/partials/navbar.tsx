f0724699a2d0c2153e0f08ea5619fc23d2de0b9e
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-02 18:10:26 +0600

    add theme toggel button

--- src/components/partials/navbar.tsx
+++ src/components/partials/navbar.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { MoonIcon, SunIcon } from '@heroicons/react/24/outline'
 
 export default function Navbar() {
     const [scrolled, setScrolled] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -25,6 +27,11 @@ export default function Navbar() {
         <nav className={`flex items-center justify-between border-b p-2 h-[80px] fixed top-0 w-full bg-white ${backdropClass}`}>
             <Link to={'/'} className="g-text font-extrabold text-2xl">JS Practice</Link>
             <div className="flex items-center justify-between gap-2">
+                <div>
+                    <div onClick={() => setIsDarkMode(!isDarkMode)} className="ghost-btn">
+                        {isDarkMode ? <MoonIcon className="h-6 w-6 text-black" />: <SunIcon className="h-6 w-6 text-black" />}
+                    </div>
+                </div>
                 <button className="btn">Add Question</button>
             </div>
         </nav>
