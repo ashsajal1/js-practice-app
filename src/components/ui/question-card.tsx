a1370d17688dfb07a63c3f4e21b3139d94949ad7
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-02 17:50:39 +0600

    show radndom questions

--- src/components/ui/question-card.tsx
+++ src/components/ui/question-card.tsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 
 export default function QuestionCard({ question, answer, id }: { question: string, answer: string, id: number }) {
     return (
-        <div className="flex flex-col rounded p-4 border w-full md:w-[300px]">
-            <p className='font-bold'>{question}</p>
-            <p className='font-light'>{answer}</p>
+        <div className="flex flex-col rounded p-4 border w-full md:min-w-[300px] md:max-w-[435px]">
+            <p className='font-bold'>{question.slice(0, 45)} {question.length > 45 ? '...' : ''}</p>
+            <p className='font-light'>{answer.slice(0, 100)}...</p>
             <div className='mt-4'>
                 <Link to={`/q/${id.toString()}`} className='btn'>Practice</Link>
             </div>
