72a536c8ad0f45a377109f6cfbb790c9cf683e30
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-02 19:43:36 +0600

    footer dark design

--- src/components/partials/footer.tsx
+++ src/components/partials/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center p-12 border-t">
+    <footer className="flex items-center justify-center p-12 border-t dark:bg-black dark:text-white dark:border-gray-700">
         All rights reserved &copy; {new Date().getFullYear()}
     </footer>
   )
