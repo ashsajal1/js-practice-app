308a33b1d11bd176215edb5fa048a6862ef8e09a
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-02 19:42:48 +0600

    show icon in dark mode

--- src/components/partials/navbar.tsx
+++ src/components/partials/navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
             <div className="flex items-center justify-between gap-2">
                 <div>
                     <div onClick={toggleMode} className="ghost-btn">
-                        {isDarkMode ? <MoonIcon className="h-6 w-6 text-black" />: <SunIcon className="h-6 w-6 text-black" />}
+                        {isDarkMode ? <MoonIcon className="h-6 w-6 text-black dark:text-white" />: <SunIcon className="h-6 w-6 text-black dark:text-white" />}
                     </div>
                 </div>
                 <button className="btn">Add Question</button>
