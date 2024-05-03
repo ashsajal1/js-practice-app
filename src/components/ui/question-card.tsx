1f0ed9aa298a2b332c293f7e609ca74d55c9dbde
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-03 15:54:33 +0600

    change size

--- src/components/ui/question-card.tsx
+++ src/components/ui/question-card.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function QuestionCard({ question, answer, id }: { question: string, answer: string, id: number }) {
     return (
-        <div data-aos='fade-up' className="flex flex-col rounded p-4 border dark:border-gray-700 w-full md:min-w-[250px] md:max-w-[400px] bg-white dark:bg-black dark:text-white">
+        <div data-aos='fade-up' className="flex flex-col rounded p-4 border dark:border-gray-700 w-full md:min-w-[350px] md:max-w-[430px] bg-white dark:bg-black dark:text-white">
             <p className='font-bold'>{question.slice(0, 40)} {question.length > 40 ? '...' : ''}</p>
             <p className='font-light'>{answer.slice(0, 80)}...</p>
             <div className='mt-4'>
