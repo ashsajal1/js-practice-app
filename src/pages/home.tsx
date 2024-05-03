99b8adf7c653d44304e30c4a87b0c2748c99da3e
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-03 15:53:05 +0600

    use grid instead flex

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,9 +12,9 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="p-4">
+    <div className="p-4 flex flex-col items-center justify-center">
       <h1 className="g-text font-extrabold text-3xl text-center pb-6">Explore questions to practice</h1>
-      <div className="flex justify-between flex-wrap items-start gap-3">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
         {sortedQuestions.map((question) => (
           <QuestionCard question={question.question} answer={question.answer} id={question.id} key={question.id} />
         ))}
