419b5d8cf17df06d6c31e9105098343a86d163a6
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-03 17:02:08 +0600

    add blob

---
+++ src/components/ui/blob.tsx
@@ -0,0 +1,9 @@
+export default function Blob() {
+    return (
+        <>
+            <div className="h-[400px] w-[400px] backdrop-blur-3xl blur-3xl bg-gradient-to-br from-blue-100 to-blue-200 rounded-full absolute left-0 top-0 -z-10"></div>
+            <div className="h-[600px] w-[600px] backdrop-blur-3xl blur-3xl bg-gradient-to-br from-blue-100 to-blue-200 rounded-full absolute top-0 -z-10"></div>
+            <div className="h-[400px] w-[400px] backdrop-blur-3xl blur-3xl bg-gradient-to-br from-blue-100 to-blue-200 rounded-full absolute right-0 top-0 -z-10"></div>
+        </>
+    )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import { useState, useMemo } from "react";
 import QuestionCard from "../components/ui/question-card";
 import { QnaTypes, qna } from "../lib/qna";
 import { getRandomSort } from "../lib/random";
+import Blob from "../components/ui/blob";
 
 export default function Home() {
   const [sortedQuestions, setSortedQuestions] = useState<QnaTypes[]>([]);
@@ -12,7 +13,8 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="p-4 flex flex-col items-center justify-center">
+    <div className="p-4 flex flex-col items-center justify-center relative">
+      <Blob />
       <h1 className="g-text font-extrabold text-3xl text-center pb-6">Explore questions to practice</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
         {sortedQuestions.map((question) => (
