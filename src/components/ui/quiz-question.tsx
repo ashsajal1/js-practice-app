b17487a817cec397646b558d658db2561e3b49bc
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-14 15:55:36 +0600

    map quiz

--- src/components/ui/quiz-question.tsx
+++ src/components/ui/quiz-question.tsx
@@ -1,5 +1,18 @@
+import { quizQuestions } from "../../lib/quiz"
+
 export default function QuizQuestion() {
-  return (
-    <div>QuizQuestion</div>
-  )
+    const question = quizQuestions[0];
+    return (
+        <div className="w-full md:w-1/3 border p-6 md:p-4 rounded">
+            <p>{question.question}</p>
+
+            {question.code && <pre><code>{question.code}</code></pre>}
+
+            <div>
+                {question.options?.map(i => <div className="p-2 rounded border my-1">{i}</div>)}
+            </div>
+
+            <div className="btn">Submit</div>
+        </div>
+    )
 }
--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,5 +1,35 @@
+// import QuizQuestion from "../components/ui/quiz-question";
+import hljs from 'highlight.js';
+import 'highlight.js/styles/default.css';
+import { useEffect } from "react";
+import { quizQuestions } from '../lib/quiz';
+
 export default function Quiz() {
-  return (
-    <div>quiz</div>
-  )
+    useEffect(() => {
+        document.querySelectorAll('pre code').forEach((block) => {
+            if (block instanceof HTMLElement) {
+                hljs.highlightBlock(block);
+            }
+        });
+    }, []);
+
+    return (
+        <>
+            {quizQuestions.map(quiz => (
+                <div>
+                    <div className="w-full md:w-1/3 border p-6 md:p-4 rounded">
+                        <p>{quiz.question}</p>
+
+                        {quiz.code && <pre><code>{quiz.code}</code></pre>}
+
+                        <div>
+                            {quiz.options?.map(i => <div className="p-2 rounded border my-1">{i}</div>)}
+                        </div>
+
+                        <div className="btn">Submit</div>
+                    </div>
+                </div>
+            ))}
+        </>
+    )
 }
