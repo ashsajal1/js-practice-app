4c1b7b9882628f6f1e7ed3d093fe2794741af2c9
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-14 16:54:56 +0600

    show question answer status in center

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -9,6 +9,8 @@ export default function Quiz() {
     const [curretnQuestion, setCurrentQuestion] = useState<QuizQuestionType | null>(null)
     const [isCompletedCurrentQuiz, setIsCompletedCurrentQuiz] = useState(false)
     const [selectedOption, setSelectedOption] = useState('')
+    const [isRightAnswer, setIsRightAnswer] = useState(false)
+
     useEffect(() => {
         document.querySelectorAll('pre code').forEach((block) => {
             if (block instanceof HTMLElement) {
@@ -18,7 +20,7 @@ export default function Quiz() {
     }, []);
 
     useEffect(() => {
-        if(quizQuestions.length > currentQuestinIndex + 1) {
+        if (quizQuestions.length > currentQuestinIndex + 1) {
             setCurrentQuestion(quizQuestions[currentQuestinIndex])
         } else {
             setCurrentQuestinIndex(0)
@@ -28,14 +30,29 @@ export default function Quiz() {
 
     if (isCompletedCurrentQuiz) {
         return <>
-            <h1>You have completed {currentQuestinIndex + 1} question!</h1>
-            <div className='btn' onClick={() => {setCurrentQuestinIndex(prevIndex => prevIndex + 1); setIsCompletedCurrentQuiz(false)}}>Practice next</div>
+            <div className='grid place-items-center'>
+                <div className="w-full md:w-1/3 border p-6 md:p-4 rounded">
+                    <h1>You have completed {currentQuestinIndex + 1} question!</h1>
+
+                    <div className={`p-4 rounded border shadow ${isRightAnswer ? 'bg-green-200' : 'bg-red-200'}`}>
+                        <p className="text-lg font-medium my-2">{curretnQuestion?.question}</p>
+                        {curretnQuestion?.options?.map((i) => (
+                            <>
+                                <p className={`p-2 text-black border rounded mb-2 cursor-pointer select-none ${curretnQuestion.answer === i ? 'bg-green-600' : ''} ${selectedOption === curretnQuestion.answer && selectedOption === i ? 'bg-green-600' : ''} ${isRightAnswer && selectedOption !== i ? 'border border-black text-black dark:border-gray-800' : ''} ${!isRightAnswer && selectedOption === i ? 'bg-red-600' : ''} ${!isRightAnswer && selectedOption !== i ? 'border border-white text-black dark:border-gray-800' : ''}`}>{i}</p>
+                            </>
+                        ))}
+
+                    </div>
+
+                    <div className='btn mt-2' onClick={() => { setCurrentQuestinIndex(prevIndex => prevIndex + 1); setIsCompletedCurrentQuiz(false) }}>Practice next</div>
+                </div>
+            </div>
         </>
     }
 
     return (
         <>
-            <div>
+            <div className='grid place-items-center mt-8'>
                 <div className="w-full md:w-1/3 border p-6 md:p-4 rounded">
                     <p>{curretnQuestion?.question}</p>
                     <p className='py-2 text-sm font-extralight'>Type : <span className='text-sm bg-blue-300 rounded p-1 text-blue-600'>{curretnQuestion?.type}</span></p>
@@ -43,10 +60,10 @@ export default function Quiz() {
                     {curretnQuestion?.code && <pre><code>{curretnQuestion.code}</code></pre>}
 
                     <div>
-                        {curretnQuestion?.options?.map(i => <div onClick={() => setSelectedOption(i)} className={`p-2 select-none cursor-pointer rounded border my-1 ${selectedOption === i ? 'border-blue-600':''}`}>{i}</div>)}
+                        {curretnQuestion?.options?.map(i => <div onClick={() => setSelectedOption(i)} className={`p-2 select-none cursor-pointer rounded border my-1 ${selectedOption === i ? 'border-blue-600' : ''}`}>{i}</div>)}
                     </div>
 
-                    <div onClick={() => { setIsCompletedCurrentQuiz(true) }} className="btn">Submit</div>
+                    <div onClick={() => { setIsCompletedCurrentQuiz(true); setIsRightAnswer((curretnQuestion?.answer === selectedOption)) }} className="btn">Submit</div>
                 </div>
             </div>
         </>
