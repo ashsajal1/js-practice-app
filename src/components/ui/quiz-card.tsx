c588b53a57c5a7974eaadb13880c9a41f62d8b86
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-15 15:05:47 +0600

    wrap quiz class in cn()

--- src/components/ui/quiz-card.tsx
+++ src/components/ui/quiz-card.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import { QuizProps } from "../../lib/qna";
 import Timer from "./timer";
+import { cn } from "../../lib/cn";
 
 export default function QuizCard({ quiz, question }: { quiz: QuizProps, question: string }) {
     const [selectedOption, setSelectedOption] = useState('')
@@ -36,7 +37,7 @@ export default function QuizCard({ quiz, question }: { quiz: QuizProps, question
                 <p className="text-lg font-medium my-2">{quiz.question}</p>
                 {quiz.options.map((i) => (
                     <>
-                        <p className={`p-2 text-black border rounded mb-2 cursor-pointer select-none ${quiz.answer === i ? 'bg-green-600' : ''} ${selectedOption === quiz.answer && selectedOption === i ? 'bg-green-600' : ''} ${isRightAnswer && selectedOption !== i ? 'border border-black text-black dark:border-gray-800' : ''} ${!isRightAnswer && selectedOption === i ? 'bg-red-600' : ''} ${!isRightAnswer && selectedOption !== i ? 'border border-white text-black dark:border-gray-800' : ''}`}>{i}</p>
+                        <p className={cn(`p-2 text-black border rounded mb-2 cursor-pointer select-none ${quiz.answer === i ? 'bg-green-600' : ''} ${selectedOption === quiz.answer && selectedOption === i ? 'bg-green-600' : ''} ${isRightAnswer && selectedOption !== i ? 'border border-black text-black dark:border-gray-800' : ''} ${!isRightAnswer && selectedOption === i ? 'bg-red-600' : ''} ${!isRightAnswer && selectedOption !== i ? 'border border-white text-black dark:border-gray-800' : ''}`)}>{i}</p>
                     </>
                 ))}
                 <div onClick={handleConfirm} className="btn w-full">Confirm or <Timer />/5</div>
@@ -49,7 +50,7 @@ export default function QuizCard({ quiz, question }: { quiz: QuizProps, question
             <p className="text-lg font-medium my-2 dark:text-white">{quiz.question}</p>
             {quiz.options.map((i) => (
                 <>
-                    <p onClick={() => setSelectedOption(i)} className={`p-2 border rounded mb-2 cursor-pointer select-none dark:border-gray-800 dark:text-white ${selectedOption === i ? 'border-blue-700 dark:border-blue-800' : ''}`}>{i}</p>
+                    <p onClick={() => setSelectedOption(i)} className={cn(`p-2 border rounded mb-2 cursor-pointer select-none dark:border-gray-800 dark:text-white ${selectedOption === i ? 'border-blue-700 dark:border-blue-800' : ''}`)}>{i}</p>
                 </>
             ))}
             <button onClick={handleQuizSubmit} className="btn w-full">Submit</button>
