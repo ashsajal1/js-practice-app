54c5d11a070a966910f3443f101766f02cc09ffd
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-17 08:22:30 +0600

    update footer

--- src/components/partials/footer.tsx
+++ src/components/partials/footer.tsx
@@ -1,7 +1,39 @@
+import { CiLinkedin, CiTwitter } from "react-icons/ci";
+import { LiaGithub } from "react-icons/lia";
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center p-12 border-t dark:bg-black dark:text-white dark:border-gray-700">
-        All rights reserved &copy; {new Date().getFullYear()}
+    <footer className="flex flex-col items-start justify-between p-4 border-t dark:bg-black dark:text-white dark:border-gray-700 w-full">
+
+      <div className="flex w-full justify-between flex-col md:flex-row gap-6">
+        <div>
+          <h3 className="font-extrabold g-text">Follow us</h3>
+          <ul>
+            <li><a className="flex items-center gap-1" href="https://www.linkedin.com/in/ashsajal/"><CiLinkedin /> Linkedin</a></li>
+            <li><a className="flex items-center gap-1" href="https://twitter.com/ashsajal1"><CiTwitter /> Twitter</a></li>
+            <li><a className="flex items-center gap-1" href="https://github.com/ashsajal1"><LiaGithub /> Twitter</a></li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="font-extrabold g-text">More Projects</h3>
+          <ul>
+            <li className=""><a href="#">Image Classification</a></li>
+            <li><a href="#">Image Classification</a></li>
+            <li><a href="#">Image Classification</a></li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="font-extrabold g-text">More Projects</h3>
+          <ul>
+            <li className=""><a href="#">Image Classification</a></li>
+            <li><a href="#">Image Classification</a></li>
+            <li><a href="#">Image Classification</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <p className="text-center w-full mt-6">All rights reserved &copy; {new Date().getFullYear()}</p>
     </footer>
   )
 }
