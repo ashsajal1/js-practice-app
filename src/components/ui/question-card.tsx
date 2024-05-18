651369f46abe310dbe1d0d2f4b2cfba5c017f044
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-18 15:34:04 +0600

    make card draggable

--- src/components/ui/question-card.tsx
+++ src/components/ui/question-card.tsx
@@ -1,15 +1,21 @@
+import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
 
 export default function QuestionCard({ question, answer, id }: { question: string, answer: string, id: number }) {
     return (
         <>
-            <div data-aos='fade-up' className="flex flex-col rounded p-4 border dark:border-gray-700 w-full md:min-w-[350px] md:max-w-[430px] bg-white dark:bg-black dark:text-white">
+            <motion.div
+                drag
+                dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+                dragElastic={0.3}
+                data-aos='fade-up'
+                className="flex flex-col rounded p-4 border dark:border-gray-700 w-full md:min-w-[350px] md:max-w-[430px] bg-white dark:bg-black dark:text-white">
                 <p className='font-bold'>{question.slice(0, 40)} {question.length > 40 ? '...' : ''}</p>
                 <p className='font-light'>{answer.slice(0, 80)}...</p>
                 <div className='mt-4'>
                     <Link to={`/practice/q/${id.toString()}`} className='btn'>Practice</Link>
                 </div>
-            </div>
+            </motion.div>
         </>
     )
 }
