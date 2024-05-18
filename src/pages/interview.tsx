bcd09496f141b037dcb41f03cad769bc2f82a6f0
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-18 19:50:38 +0600

    resolve layout

--- src/pages/interview.tsx
+++ src/pages/interview.tsx
@@ -25,11 +25,11 @@ export default function Interview() {
     };
 
     return (
-        <div className="p-4">
-            <div className="mb-4 flex flex-col gap-2">
+        <div className="p-4 w-full">
+            <div className="mb-4 flex flex-col gap-2 w-full">
                 {messages.map((message, index) => (
-                    <div key={index} className={`flex ${message.user === 'User' ? 'justify-end' : 'justify-start'}`}>
-                        <div className='flex gap-2'>
+                    <div key={index} className={`flex w-full ${message.user === 'User' ? 'justify-end' : 'justify-start'}`}>
+                        <div className='flex gap-2 w-3/4'>
                             {message.user === 'Robot' &&<div className='flex items-center justify-center w-[40px] h-[40px] p-1 bg-black rounded-full'></div>}
                             <Message text={message.text} />
                             {message.user === 'User' &&<div className='w-[40px] h-[40px] p-1 bg-black rounded-full'></div>}
