90d930289d9608d5a302fc94669cd8c74e8036ec
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-19 17:03:16 +0600

    resovle justify end

--- src/pages/interview.tsx
+++ src/pages/interview.tsx
@@ -57,11 +57,12 @@ export default function Interview() {
         <div className="p-4 w-full">
             <div className="mb-4 flex flex-col gap-2 w-full">
                 {messages.map((message, index) => (
-                    <div key={index} className={`flex flex-col w-full ${message.user === 'User' ? 'justify-end' : 'justify-start'}`}>
-                        <div className='flex gap-2 w-3/4'>
-                            {message.user === 'Robot' && <div className='flex items-center justify-center w-[40px] h-[40px] p-1 bg-black rounded-full'></div>}
-
-                            <div className='w-full'>
+                    <div key={index} className={`flex w-full ${message.user === 'User' ? 'justify-end' : 'justify-start'}`}>
+                        <div className={`flex gap-2 w-3/4 ${message.user === 'User' ? 'items-end' : 'items-start'}`}>
+                            {message.user === 'Robot' && (
+                                <div className="flex items-center justify-center w-[40px] h-[40px] p-1 bg-black rounded-full"></div>
+                            )}
+                            <div className="w-full">
                                 <Message text={message.text} />
                                 {message.options && (
                                     <div className="flex gap-2 flex-wrap">
@@ -77,10 +78,10 @@ export default function Interview() {
                                     </div>
                                 )}
                             </div>
-
-                            {message.user === 'User' && <div className='w-[40px] h-[40px] p-1 bg-black rounded-full'></div>}
+                            {message.user === 'User' && (
+                                <div className="w-[40px] h-[40px] p-1 bg-black rounded-full"></div>
+                            )}
                         </div>
-
                     </div>
                 ))}
             </div>
