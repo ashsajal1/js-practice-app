930815f0748c5752566f7fd61da514f3226261f2
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-20 10:16:51 +0600

    add margin in user text

--- src/pages/interview.tsx
+++ src/pages/interview.tsx
@@ -86,7 +86,7 @@ export default function Interview() {
         <div className="p-4 w-full">
             <div className="mb-4 flex flex-col gap-2 w-full">
                 {messages.map((message, index) => (
-                    <div key={index} className={`flex w-full ${message.user === 'User' ? 'justify-end' : 'justify-start'}`}>
+                    <div key={index} className={`flex w-full ${message.user === 'User' ? 'justify-end my-6' : 'justify-start'}`}>
                         <div className={`flex gap-2 w-full md:w-3/4 ${message.user === 'User' ? 'items-end' : 'items-start'}`}>
                             {message.user === 'Robot' && (
                                 <img className='[40px] h-[40px] rounded-full contain-content' src="/image/interviewer.jpg" alt="interviewer" />
