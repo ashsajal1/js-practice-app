ca0be84303c52a5a435aebb89166680ff3e69184
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-20 09:35:46 +0600

    enhance readablity

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,4 +1,3 @@
-// import QuizQuestion from "../components/ui/quiz-question";
 import hljs from 'highlight.js';
 import 'highlight.js/styles/default.css';
 import { useEffect, useState } from "react";
@@ -8,16 +7,16 @@ import AnimatedPage from '../components/ui/animated-page';
 import { getRandomSort } from '../lib/random';
 
 export default function Quiz() {
-    const [currentQuestinIndex, setCurrentQuestinIndex] = useState(0);
-    const [curretnQuestion, setCurrentQuestion] = useState<QuizQuestionType | null>(null)
-    const [isCompletedCurrentQuiz, setIsCompletedCurrentQuiz] = useState(false)
-    const [selectedOption, setSelectedOption] = useState('')
-    const [isRightAnswer, setIsRightAnswer] = useState(false)
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState<QuizQuestionType | null>(null);
+    const [isCompletedCurrentQuiz, setIsCompletedCurrentQuiz] = useState(false);
+    const [selectedOption, setSelectedOption] = useState('');
+    const [isRightAnswer, setIsRightAnswer] = useState(false);
     const [questions, setQuestions] = useState<QuizQuestionType[]>([]);
 
     useEffect(() => {
-        setQuestions(quizQuestions.sort(getRandomSort).slice(0, 5))
-    }, [])
+        setQuestions(quizQuestions.sort(getRandomSort).slice(0, 5));
+    }, []);
 
     useEffect(() => {
         document.querySelectorAll('pre code').forEach((block) => {
@@ -25,59 +24,80 @@ export default function Quiz() {
                 hljs.highlightBlock(block);
             }
         });
-    }, [currentQuestinIndex]);
+    }, [currentQuestionIndex]);
 
     useEffect(() => {
-        if (questions.length > currentQuestinIndex) {
-            setCurrentQuestion(questions[currentQuestinIndex])
+        if (questions.length > currentQuestionIndex) {
+            setCurrentQuestion(questions[currentQuestionIndex]);
         } else {
-            setCurrentQuestinIndex(0)
-            setCurrentQuestion(questions[0])
+            setCurrentQuestionIndex(0);
+            setCurrentQuestion(questions[0]);
         }
-    }, [currentQuestinIndex, questions]);
+    }, [currentQuestionIndex, questions]);
+
+    const handleNextQuestion = () => {
+        setCurrentQuestionIndex(prevIndex => prevIndex + 1);
+        setIsCompletedCurrentQuiz(false);
+        setIsRightAnswer(false);
+        setSelectedOption('');
+    };
+
+    const handleSubmit = () => {
+        setIsCompletedCurrentQuiz(true);
+        setIsRightAnswer(currentQuestion?.answer === selectedOption);
+    };
 
     if (isCompletedCurrentQuiz) {
-        return <>
+        return (
             <AnimatedPage>
-                <div className='grid place-items-center pt-12 px-4  md:p-12 pb-24'>
+                <div className='grid place-items-center pt-12 px-4 md:p-12 pb-24'>
                     <div className="w-full md:w-1/3 border dark:border-gray-800 p-6 md:p-4 rounded">
-                        {/* <h1>You have completed {currentQuestinIndex + 1} question!</h1> */}
-
                         <div className={`p-4 rounded border shadow ${isRightAnswer ? 'bg-green-200' : 'bg-red-200'}`}>
-                            <p className="text-lg font-medium my-2">{curretnQuestion?.question}</p>
-                            {curretnQuestion?.options?.map((i) => (
-                                <>
-                                    <p className={`p-2 text-black border border-black rounded mb-2 cursor-pointer select-none ${curretnQuestion.answer === i ? 'bg-green-600' : ''} ${selectedOption === curretnQuestion.answer && selectedOption === i ? 'bg-green-600' : ''} ${!isRightAnswer && selectedOption === i ? 'bg-red-600' : ''} `}>{i}</p>
-                                </>
+                            <p className="text-lg font-medium my-2">{currentQuestion?.question}</p>
+                            {currentQuestion?.options?.map((option, index) => (
+                                <p
+                                    key={index}
+                                    className={`p-2 text-black border border-black rounded mb-2 cursor-pointer select-none
+                                        ${currentQuestion.answer === option ? 'bg-green-600' : ''}
+                                        ${selectedOption === currentQuestion.answer && selectedOption === option ? 'bg-green-600' : ''}
+                                        ${!isRightAnswer && selectedOption === option ? 'bg-red-600' : ''}`}
+                                >
+                                    {option}
+                                </p>
                             ))}
-
                         </div>
-
-                        <div className='btn mt-2' onClick={() => { setCurrentQuestinIndex(prevIndex => prevIndex + 1); setIsCompletedCurrentQuiz(false); setIsRightAnswer(false) }}>Practice next</div>
+                        <div className='btn mt-2' onClick={handleNextQuestion}>Practice next</div>
                     </div>
                 </div>
             </AnimatedPage>
-        </>
+        );
     }
 
     return (
-        <>
-            <AnimatedPage>
-                <div className='grid place-items-center pt-12 px-4  md:p-12 pb-24'>
-                    <div className="w-full md:w-1/3 border dark:border-gray-800 p-6 md:p-4 rounded">
-                        <p className='dark:text-white'>{curretnQuestion?.question}</p>
-                        <p className='py-2 text-sm font-extralight dark:text-white'>Type : <span className='text-sm bg-blue-300 rounded p-1 text-blue-600'>{curretnQuestion?.type}</span></p>
-
-                        <pre><code>{curretnQuestion?.code}</code></pre>
-
-                        <div>
-                            {curretnQuestion?.options?.map(i => <div onClick={() => setSelectedOption(i)} className={cn(`p-2 dark:text-white select-none cursor-pointer rounded border my-1 dark:border-gray-800 ${selectedOption === i ? 'border-blue-600 dark:border-blue-600' : ''}`)}>{i}</div>)}
-                        </div>
-
-                        <div onClick={() => { setIsCompletedCurrentQuiz(true); setIsRightAnswer((curretnQuestion?.answer === selectedOption)) }} className="btn">Submit</div>
+        <AnimatedPage>
+            <div className='grid place-items-center pt-12 px-4 md:p-12 pb-24'>
+                <div className="w-full md:w-1/3 border dark:border-gray-800 p-6 md:p-4 rounded">
+                    <p className='dark:text-white'>{currentQuestion?.question}</p>
+                    <p className='py-2 text-sm font-extralight dark:text-white'>
+                        Type: <span className='text-sm bg-blue-300 rounded p-1 text-blue-600'>{currentQuestion?.type}</span>
+                    </p>
+                    <pre><code>{currentQuestion?.code}</code></pre>
+                    <div>
+                        {currentQuestion?.options?.map((option, index) => (
+                            <div
+                                key={index}
+                                onClick={() => setSelectedOption(option)}
+                                className={cn(`p-2 dark:text-white select-none cursor-pointer rounded border my-1 dark:border-gray-800 
+                                    ${selectedOption === option ? 'border-blue-600 dark:border-blue-600' : ''}`)}
+                            >
+                                {option}
+                            </div>
+                        ))}
                     </div>
+                    <div onClick={handleSubmit} className="btn">Submit</div>
                 </div>
-            </AnimatedPage>
-        </>
-    )
+            </div>
+        </AnimatedPage>
+    );
 }
+
