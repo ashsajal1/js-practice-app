6aebb62558bbee9941a3f91db5f486da2b721810
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-21 22:41:40 +0600

    create topic slice

---
+++ src/features/topics/topic/topicSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { qna, QnaTypes } from "../../../lib/qna";
+import { getRandomSort } from "../../../lib/random";
+
+interface TopicState {
+  topics: QnaTypes[];
+}
+
+const initialState: TopicState = {
+  topics: [],
+};
+
+const topicSlice = createSlice({
+  name: "topic",
+  initialState,
+  reducers: {
+    getRandomTopics: (state) => {
+      const randomTopics = generateRandomTopics();
+      state.topics = randomTopics;
+    },
+  },
+});
+
+export const { getRandomTopics } = topicSlice.actions;
+
+export default topicSlice.reducer;
+
+const generateRandomTopics = (): QnaTypes[] => {
+  const randomTopics = qna.slice().sort(getRandomSort);
+  return randomTopics;
+};
