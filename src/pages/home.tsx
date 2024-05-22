723b17a2b383f9b42a55553c528c598555dc1f04
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-22 12:35:31 +0600

    design serach for dark mode

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -19,12 +19,12 @@ export default function Home() {
   return (
     <AnimatedPage>
       <section className="p-12 flex items-center justify-center">
-        <form onSubmit={handleSearch} className="border flex items-center justify-between rounded px-2 w-1/2 md:w-1/3 gap-2 p-1 focus-within:border-blue-600">
+        <form onSubmit={handleSearch} className="border dark:border-gray-700 flex items-center justify-between rounded px-2 w-1/2 md:w-1/3 gap-2 p-1 focus-within:border-blue-600">
           <input
             onChange={(e) => setTopic(e.target.value)}
             type="text"
             placeholder="Rust, Golang, React"
-            className="p-2 rounded outline-none"
+            className="p-2 dark:bg-black dark:text-white rounded outline-none"
           />
 
           <Button>
