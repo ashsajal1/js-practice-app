984c374cf07524803a8ecefc850fc3633337d1ea
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-22 14:51:06 +0600

    create counter stat

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,7 +6,7 @@ import Button from "../components/ui/button";
 import { ChangeEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import TopicBadge from "../components/ui/topic-badge";
-import Counter from "../components/ui/counter";
+import CounterStat from "../components/partials/counter-stat";
 
 export default function Home() {
   const [topic, setTopic] = useState('all');
@@ -42,20 +42,7 @@ export default function Home() {
           <TopicBadge topic="golang" />
         </div>
 
-        <div className="flex items-center">
-          <div className="flex flex-col gap-2 justify-center items-center p-4 shadow">
-            <Counter className="text-blue-700 text-2xl" value={129} />
-            <span className="font-bold"><span className="text-blue-600">Concepts</span> to Practice</span>
-          </div>
-          <div className="flex flex-col gap-2 justify-center items-center p-4 shadow">
-            <Counter className="text-blue-700 text-2xl" speed={20} value={201} />
-            <span className="font-bold "><span className="text-blue-600">People</span> has used</span>
-          </div>
-          <div className="flex flex-col gap-2 justify-center items-center p-4 shadow">
-            <Counter className="text-blue-700 text-2xl" speed={18} value={207} />
-            <span className="font-bold"><span className="text-blue-600">Quizzes</span> to Play</span>
-          </div>
-        </div>
+        <CounterStat />
       </section>
 
       <div className="p-4 flex flex-col items-center justify-center relative">
