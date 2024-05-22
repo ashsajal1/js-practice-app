1a65032f33a4eac07d42e4ff0441fb8739f3664a
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-22 21:01:49 +0600

    change url destination

--- src/components/ui/question-card.tsx
+++ src/components/ui/question-card.tsx
@@ -10,7 +10,7 @@ export default function QuestionCard({ question, answer, id }: { question: strin
                 <p className='font-bold'>{question.slice(0, 40)} {question.length > 40 ? '...' : ''}</p>
                 <p className='font-light'>{answer.slice(0, 80)}...</p>
                 <div className='mt-4'>
-                    <Link to={`/practice/q/${id.toString()}`} className='btn'>Practice</Link>
+                    <Link to={`/practice?id=${id.toString()}`} className='btn'>Practice</Link>
                 </div>
             </motion.div>
         </>
