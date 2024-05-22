a2bbc945a1eb6976d92c1030fa75d630e0c3dc42
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-22 11:39:20 +0600

    create search

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,8 @@
 import QuestionCard from "../components/ui/question-card";
 import AnimatedPage from "../components/ui/animated-page";
 import { useTypedSelector } from "../hooks/useTypedSelector";
+import { CiSearch } from "react-icons/ci";
+import Button from "../components/ui/button";
 
 export default function Home() {
 
@@ -9,6 +11,16 @@ export default function Home() {
 
   return (
     <AnimatedPage>
+
+      <section className="p-12 flex items-center justify-center">
+        <div className="border flex items-center justify-between rounded px-2 w-1/2 md:w-1/3 gap-2 p-1 focus-within:border-blue-600">
+          <input type="text" placeholder="Rust, Golang, React" className="p-2 rounded outline-none" />
+
+          <Button>
+            <CiSearch className="h-4 w-4" />
+          </Button>
+        </div>
+      </section>
       <div className="p-4 flex flex-col items-center justify-center relative">
         <h1 className="g-text font-extrabold text-3xl text-center pb-6">Explore questions to practice</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
