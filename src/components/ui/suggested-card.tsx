2aee6fcf37d9fed450fed99779fe95a18915f888
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-22 21:02:14 +0600

    change url

--- src/components/ui/suggested-card.tsx
+++ src/components/ui/suggested-card.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function SuggestedCard({ id, question, answer }: { id: number, question: string, answer: string }) {
     return (
-        <Link data-aos='fade-up' to={`/practice/q/${id.toString()}`} className="flex flex-col rounded border text-sm p-2 min-w-[280px] h-[90px]  md:w-[380px] bg-white dark:bg-black dark:border-gray-700 dark:text-white">
+        <Link data-aos='fade-up' to={`/practice?id=${id.toString()}`} className="flex flex-col rounded border text-sm p-2 min-w-[280px] h-[90px]  md:w-[380px] bg-white dark:bg-black dark:border-gray-700 dark:text-white">
             <p className="font-bold">{question.slice(0, 50)}</p>
             <p>{answer.slice(0, 50)}</p>
         </Link>
