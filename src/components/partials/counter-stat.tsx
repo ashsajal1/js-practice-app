866406245ef1e60546f45a7f2a58f1862b365854
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-22 15:13:25 +0600

    design counter stat for dark mode

--- src/components/partials/counter-stat.tsx
+++ src/components/partials/counter-stat.tsx
@@ -2,16 +2,16 @@ import Counter from "../ui/counter";
 
 export default function CounterStat() {
     return (
-        <div className="flex items-center">
-            <div className="flex flex-col gap-2 justify-center items-center p-4 shadow">
+        <div className="flex items-center dark:text-white">
+            <div className="flex flex-col gap-2 justify-center items-center p-4 shadow dark:shadow-white dark:shadow-sm">
                 <Counter className="text-blue-700 text-2xl" value={129} />
                 <span className="font-bold"><span className="text-blue-600">Concepts</span> to Practice</span>
             </div>
-            <div className="flex flex-col gap-2 justify-center items-center p-4 shadow">
+            <div className="flex flex-col gap-2 justify-center items-center p-4 shadow dark:shadow-white dark:shadow-sm">
                 <Counter className="text-blue-700 text-2xl" speed={20} value={201} />
                 <span className="font-bold "><span className="text-blue-600">People</span> have used</span>
             </div>
-            <div className="flex flex-col gap-2 justify-center items-center p-4 shadow">
+            <div className="flex flex-col gap-2 justify-center items-center p-4 shadow dark:shadow-white dark:shadow-sm">
                 <Counter className="text-blue-700 text-2xl" speed={18} value={207} />
                 <span className="font-bold"><span className="text-blue-600">Quizzes</span> to Play</span>
             </div>
