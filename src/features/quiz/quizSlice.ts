fa8c2399eac2db0560a21aa6687c18b7b8a8e425
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-24 10:38:31 +0600

    add loading and error propos

--- src/features/quiz/quizSlice.ts
+++ src/features/quiz/quizSlice.ts
@@ -4,10 +4,14 @@ import { generateRandomQuizzes } from "../../lib/random";
 
 export interface QuizState {
   quizzes: QuizQuestionType[];
+  loading: boolean;
+  error: string | null;
 }
 
 const initialState: QuizState = {
   quizzes: [],
+  loading: false,
+  error: null,
 };
 
 const quizSlice = createSlice({
@@ -15,11 +19,17 @@ const quizSlice = createSlice({
   initialState,
   reducers: {
     getAllQuiz: (state) => {
+      state.loading = true;
+      state.error = null;
       state.quizzes = generateRandomQuizzes;
+      state.loading = false;
     },
     getAllQuizByTopic: (state, action: PayloadAction<string>) => {
+      state.loading = true;
+      state.error = null;
       const topic = action.payload;
       state.quizzes = generateRandomQuizzes.filter((i) => i.topic === topic);
+      state.loading = false;
     },
   },
 });
