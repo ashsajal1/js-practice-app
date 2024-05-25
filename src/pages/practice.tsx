1c1e07d765d5f58d792a0bac62f69d1ca26fef0e
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-25 15:17:10 +0600

    alert(transcript)

--- src/pages/practice.tsx
+++ src/pages/practice.tsx
@@ -67,6 +67,7 @@ export default function Practice() {
 
   useEffect(() => {
     if (transcript) {
+      alert(transcript)
       setChar(prevChar => prevChar + transcript);
     }
   }, [transcript]);
