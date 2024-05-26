0a7180fbc5cb38871ce883b9c6f0c2ca1ee5aa97
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-26 15:42:06 +0600

    use quiz question from redux slice

--- src/pages/interview.tsx
+++ src/pages/interview.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
 import Message from '../components/ui/message';
-import { AnimatePresence, motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion";
 import Button from '../components/ui/button';
 import { getRandomSort } from '../lib/random';
-import { quizQuestions } from '../lib/quizzes/javascript';
-import { QuizQuestionType } from '../lib/quizzes/types';
 import { useTextToSpeech } from '../hooks/useTextToSpeech';
+import { useDispatch } from 'react-redux';
+import { getAllQuiz } from '../features/quiz/quizSlice';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 type MessageType = {
     user: string;
@@ -17,17 +18,18 @@ type MessageType = {
 
 export default function Interview() {
     const [messages, setMessages] = useState<MessageType[]>([]);
-    const [questions, setQuestions] = useState<QuizQuestionType[]>([]);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [score, setScore] = useState(0);
     const [isBotWriting, setIsBotWriting] = useState(false);
     const lastMessageRef = useRef<HTMLDivElement>(null);
-    const { speak } = useTextToSpeech()
+    const { speak } = useTextToSpeech();
+    const dispatch = useDispatch();
 
-    // console.log(questions)
     useEffect(() => {
-        setQuestions(quizQuestions.sort(getRandomSort).slice(0, 5))
-    }, [])
+        dispatch(getAllQuiz());
+    }, [dispatch]);
+
+    const { quizzes, loading } = useTypedSelector((state) => state.quiz);
 
     useEffect(() => {
         if (lastMessageRef.current) {
@@ -36,17 +38,25 @@ export default function Interview() {
     }, [messages]);
 
     useEffect(() => {
-        if (messages.length > 0) {
-            if (messages[messages.length - 1].user === "Robot") {
-                speak(messages[messages.length - 1].text)
-            }
+        if (messages.length > 0 && messages[messages.length - 1].user === "Robot") {
+            speak(messages[messages.length - 1].text);
         }
-    }, [messages, speak])
+    }, [messages, speak]);
 
     const startQuiz = () => {
-        if (questions.length > 0) {
-            const firstQuestion = questions[0];
-            setMessages([{ user: 'Robot', text: firstQuestion.question, options: firstQuestion.options, answered: false, code: firstQuestion.code }]);
+        if (quizzes.length > 0) {
+            // Limit the number of questions to 5
+            const shuffledQuizzes = [...quizzes].sort(getRandomSort).slice(0, 5);
+            const firstQuestion = shuffledQuizzes[0];
+            setMessages([
+                {
+                    user: 'Robot',
+                    text: firstQuestion.question,
+                    options: firstQuestion.options,
+                    answered: false,
+                    code: firstQuestion.code,
+                },
+            ]);
             setCurrentQuestionIndex(0);
             setScore(0);
         }
@@ -55,17 +65,15 @@ export default function Interview() {
     const handleOptionSelect = (index: number) => {
         if (isBotWriting) return;
 
-        const currentQuestion = questions[currentQuestionIndex];
+        const currentQuestion = quizzes[currentQuestionIndex];
         const selectedOption = currentQuestion.options ? currentQuestion.options[index] : "";
         const isCorrect = selectedOption === currentQuestion.answer;
         const feedback = isCorrect ? "Correct!" : `Incorrect. The correct answer was "${currentQuestion.answer}"`;
-        speak(feedback)
+        speak(feedback);
         const newScore = isCorrect ? score + 1 : score;
-        const isLastQuestion = currentQuestionIndex === questions.length - 1;
+        const isLastQuestion = currentQuestionIndex === 4; 
 
-        const updatedMessages = messages.map((message, idx) => (
-            idx === messages.length - 1 ? { ...message, answered: true } : message
-        ));
+        const updatedMessages = messages.map((message, idx) => (idx === messages.length - 1 ? { ...message, answered: true } : message));
 
         const newMessages = [
             ...updatedMessages,
@@ -76,23 +84,35 @@ export default function Interview() {
         setIsBotWriting(true);
 
         setTimeout(() => {
-            const botMessages: MessageType[] = [
-                { user: 'Robot', text: feedback }
-            ];
+            const botMessages: MessageType[] = [{ user: 'Robot', text: feedback }];
 
             if (isLastQuestion) {
-                botMessages.push({ user: 'Robot', text: `Interview finished! Your score: ${newScore}/${questions.length}` });
+                botMessages.push({ user: 'Robot', text: `Interview finished! Your score: ${newScore}/5` });
             } else {
-                const nextQuestion = questions[currentQuestionIndex + 1];
-                botMessages.push({ user: 'Robot', text: nextQuestion.question, options: nextQuestion.options, answered: false, code: nextQuestion.code });
+                const nextQuestion = quizzes[currentQuestionIndex + 1];
+                botMessages.push({
+                    user: 'Robot',
+                    text: nextQuestion.question,
+                    options: nextQuestion.options,
+                    answered: false,
+                    code: nextQuestion.code,
+                });
             }
 
             setMessages([...newMessages, ...botMessages]);
             setScore(newScore);
-            setCurrentQuestionIndex(currentQuestionIndex + 1);
+            setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
             setIsBotWriting(false);
         }, 1000);
     };
+
+    if (loading) {
+        return (
+            <div>
+                <div>Loading</div>
+            </div>
+        );
+    }
 
     return (
         <div className="p-4 w-full">
@@ -101,7 +121,7 @@ export default function Interview() {
                     <div key={index} className={`flex w-full ${message.user === 'User' ? 'justify-end my-6' : 'justify-start'}`}>
                         <div className={`flex gap-2 w-full md:w-3/4 ${message.user === 'User' ? 'items-end' : 'items-start'}`}>
                             {message.user === 'Robot' && (
-                                <img className='[40px] h-[40px] rounded-full contain-content' src="/image/interviewer.jpg" alt="interviewer" />
+                                <img className="[40px] h-[40px] rounded-full contain-content" src="/image/interviewer.jpg" alt="interviewer" />
                             )}
                             <div ref={lastMessageRef} className="w-full">
                                 <Message className={`${message.user === 'Robot' ? 'bg-gradient-to-br from-blue-600 via-blue-700 to-blue-800 text-white border-none' : 'bg-gradient-to-br from-gray-800 via-gray-900 to-gray-950 text-white border-none'}`} text={message.text} />
@@ -113,13 +133,13 @@ export default function Interview() {
                                             initial={{ scale: 0 }}
                                             animate={{ scale: 1 }}
                                             transition={{
-                                                duration: 0.3
+                                                duration: 0.3,
                                             }}
                                             className="flex gap-2 flex-wrap"
                                         >
-                                            {message.options.sort(getRandomSort).map((option, optionIndex) => (
+                                            {message.options.slice().sort(getRandomSort).map((option, optionIndex) => (
                                                 <Button
-                                                    variant='outline'
+                                                    variant="outline"
                                                     key={optionIndex}
                                                     className="border p-2 mt-2"
                                                     onClick={() => handleOptionSelect(optionIndex)}
@@ -132,7 +152,7 @@ export default function Interview() {
                                 </AnimatePresence>
                             </div>
                             {message.user === 'User' && (
-                                <img className='[40px] h-[40px] rounded-full' src="/image/user.jpg" alt="user" />
+                                <img className="[40px] h-[40px] rounded-full" src="/image/user.jpg" alt="user" />
                             )}
                         </div>
                     </div>
