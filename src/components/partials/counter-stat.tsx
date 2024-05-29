605efad5b93792b4c5fed9814ff1fa3e0a15288f
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-29 23:09:17 +0600

    center text

--- src/components/partials/counter-stat.tsx
+++ src/components/partials/counter-stat.tsx
@@ -5,15 +5,15 @@ export default function CounterStat() {
         <div className="flex items-center dark:text-white">
             <div className="flex flex-col gap-2 justify-center items-center p-4 shadow dark:shadow-white dark:shadow-sm">
                 <Counter className="text-blue-700 text-2xl" value={129} />
-                <span className="font-bold"><span className="text-blue-600">Concepts</span> to Practice</span>
+                <span className="font-bold text-center"><span className="text-blue-600">Concepts</span> to Practice</span>
             </div>
             <div className="flex flex-col gap-2 justify-center items-center p-4 shadow dark:shadow-white dark:shadow-sm">
                 <Counter className="text-blue-700 text-2xl" speed={20} value={201} />
-                <span className="font-bold "><span className="text-blue-600">People</span> have used</span>
+                <span className="font-bold text-center"><span className="text-blue-600">People</span> have used</span>
             </div>
             <div className="flex flex-col gap-2 justify-center items-center p-4 shadow dark:shadow-white dark:shadow-sm">
                 <Counter className="text-blue-700 text-2xl" speed={18} value={207} />
-                <span className="font-bold"><span className="text-blue-600">Quizzes</span> to Play</span>
+                <span className="font-bold text-center"><span className="text-blue-600">Quizzes</span> to Play</span>
             </div>
         </div>
     )
