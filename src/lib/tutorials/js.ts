292f5cd1ee0deb6ba2f4c2485baa36dfe2eb10a0
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 16:29:58 +0600

    create array of js tutorials

---
+++ src/lib/tutorials/js.ts
@@ -0,0 +1,42 @@
+export const jsTutorials: { videoUrl: string; topic: string }[] = [
+    {
+      videoUrl: "https://www.youtube.com/watch?v=PkZNo7MFNFg",
+      topic: "Introduction to JavaScript",
+    },
+    {
+      videoUrl: "https://www.youtube.com/watch?v=hdI2bqOjy3c",
+      topic: "Variables and Data Types in JavaScript",
+    },
+    {
+      videoUrl: "https://www.youtube.com/watch?v=Ukg_U3CnJWI",
+      topic: "Operators and Expressions in JavaScript",
+    },
+    {
+      videoUrl: "https://www.youtube.com/watch?v=Bv_5Zv5c-Ts",
+      topic: "Control Flow in JavaScript",
+    },
+    {
+      videoUrl: "https://www.youtube.com/watch?v=_PqqB-UQ_p0",
+      topic: "Functions in JavaScript",
+    },
+    {
+      videoUrl: "https://www.youtube.com/watch?v=8X2kIfS6fb8",
+      topic: "Arrays in JavaScript",
+    },
+    {
+      videoUrl: "https://www.youtube.com/watch?v=NcoBAfJ6l2Q",
+      topic: "Objects in JavaScript",
+    },
+    {
+      videoUrl: "https://www.youtube.com/watch?v=Mus_vwhTCq0",
+      topic: "DOM Manipulation in JavaScript",
+    },
+    {
+      videoUrl: "https://www.youtube.com/watch?v=GhQdlIFylQ8",
+      topic: "Events in JavaScript",
+    },
+    {
+      videoUrl: "https://www.youtube.com/watch?v=ukoMrWnUmcg",
+      topic: "Asynchronous JavaScript",
+    },
+  ];
