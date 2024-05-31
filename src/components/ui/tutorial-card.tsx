8ed2a8e51ec241d60452b413ed67096f6685dab5
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 15:01:58 +0600

    show yt video thumnail

--- src/components/ui/tutorial-card.tsx
+++ src/components/ui/tutorial-card.tsx
@@ -4,7 +4,7 @@ export default function TutorialCard() {
     return (
         <div className="p-2 rounded border dark:border-gray-700 shadow min-w-[40px]">
             <h3 className="font-bold text-lg dark:text-white">Javascript Tutorial</h3>
-            <div className="w-full bg-gray-600 h-[120px]"></div>
+            <div className={`w-full rounded bg-cover bg-[url('https://img.youtube.com/vi/BVUTkSAf-6w/maxresdefault.jpg')] h-[120px]`}></div>
             <div className="mt-2">
                 <Button className="w-full">Watch</Button>
             </div>
