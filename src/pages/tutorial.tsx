38cf97a36680df789075f3121c09137cd2237fdb
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 16:42:28 +0600

    imort video from array

--- src/pages/tutorial.tsx
+++ src/pages/tutorial.tsx
@@ -1,17 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import TutorialCard from "../components/ui/tutorial-card";
+import { jsTutorials } from "../lib/tutorials/js";
 
 export default function Tutorial() {
     const navigate = useNavigate()
-    const handleWatch = () => {
-        navigate(`/watch?id=BVUTkSAf-6w`)
+    const handleWatch = (id: string) => {
+        navigate(`/watch?id=${id}`)
     }
     return (
         <div className="p-4">
             <h1 className="text-2xl g-text font-bold text-center py-2 border-b border-b-blue-600 mb-4">Explore Video Tutorials</h1>
 
             <section className="grid grid-cols-3 gap-2">
-                <TutorialCard videoId="BVUTkSAf-6w" handleWatch={handleWatch} />
+                {jsTutorials.map(video => (
+                    <TutorialCard videoId={video.videoId} handleWatch={() => handleWatch(video.videoId)} />
+                ))}
+
             </section>
         </div>
     )
