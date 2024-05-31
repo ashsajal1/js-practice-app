847c45ad72a3fc46498a9df462dcf26dc16ce939
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 15:46:30 +0600

    navigate to watch page

--- src/components/ui/tutorial-card.tsx
+++ src/components/ui/tutorial-card.tsx
@@ -1,13 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Button from "./button";
 
 export default function TutorialCard() {
-    return (
-        <div className="p-2 rounded border dark:border-gray-700 shadow min-w-[40px]">
-            <h3 className="font-bold text-lg dark:text-white">Javascript Tutorial</h3>
-            <div className={`w-full rounded bg-cover bg-[url('https://img.youtube.com/vi/BVUTkSAf-6w/maxresdefault.jpg')] h-[120px] md:h-[200px]`}></div>
-            <div className="mt-2">
-                <Button className="w-full">Watch</Button>
-            </div>
-        </div>
-    )
+  const navigate = useNavigate()
+  const handleWatch = () => {
+    console.log("watch")
+    navigate(`watch?id=${'BVUTkSAf-6w'}`)
+  }
+  return (
+    <div className="p-2 rounded border dark:border-gray-700 shadow min-w-[40px]">
+      <h3 className="font-bold text-lg dark:text-white">Javascript Tutorial</h3>
+      <div className={`w-full rounded bg-cover bg-[url('https://img.youtube.com/vi/BVUTkSAf-6w/maxresdefault.jpg')] h-[120px] md:h-[200px]`}></div>
+      <div onClick={handleWatch} className="mt-2">
+        <Button className="w-full">Watch</Button>
+      </div>
+    </div>
+  )
 }
