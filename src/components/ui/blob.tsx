92bed5abcdb831b5ae9cb987497ea5a889d193cf
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-04 16:16:14 +0600

    take any props in blob

--- src/components/ui/blob.tsx
+++ src/components/ui/blob.tsx
@@ -1,9 +1,11 @@
-export default function Blob() {
+import { cn } from "../../lib/cn";
+
+export default function Blob({ ...props }) {
+    const { className } = props
     return (
         <>
-            <div className="h-[400px] w-[400px] backdrop-blur-3xl blur-3xl bg-gradient-to-br from-blue-100 to-blue-200 rounded-full absolute left-0 top-0 -z-10"></div>
-            <div className="h-[600px] w-[600px] backdrop-blur-3xl blur-3xl bg-gradient-to-br from-blue-100 to-blue-200 rounded-full absolute top-0 -z-10"></div>
-            <div className="h-[400px] w-[400px] backdrop-blur-3xl blur-3xl bg-gradient-to-br from-blue-100 to-blue-200 rounded-full absolute right-0 top-0 -z-10"></div>
+            <div {...props} className={cn("h-[400px] w-[400px] backdrop-blur-3xl blur-3xl bg-gradient-to-br from-blue-100 to-blue-200 rounded-full absolute -z-10", className)}></div>
+
         </>
     )
 }
