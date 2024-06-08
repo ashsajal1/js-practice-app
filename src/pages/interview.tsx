571643fc2219ac7aa04f991914839e9a27fef598
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-08 14:51:02 +0600

    style: take full width

--- src/pages/interview.tsx
+++ src/pages/interview.tsx
@@ -208,7 +208,7 @@ export default function Interview() {
                     </div>
                 ))}
                 {isLastQuestion && <div className={`py-2 w-full flex items-center justify-center`}>
-                    <Button onClick={() => window.location.reload()} variant='outline' className='w-1/2 md:w-1/3 gap-2'>
+                    <Button onClick={() => window.location.reload()} variant='outline' className='w-full md:w-1/3 gap-2'>
                         <HiRefresh />
                         Restart
                     </Button>
