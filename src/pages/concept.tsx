913a983739506d72fed2b5006aed7ce2d9e33fb3
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-09 10:11:56 +0600

    refactor: fix typo

--- src/pages/concept.tsx
+++ src/pages/concept.tsx
@@ -38,7 +38,7 @@ export default function Concept() {
                     <HiMiniXMark onClick={() => {history.back()}} className="h-6 w-6 dark:text-white" />
                 </div>}
 
-                {filteredConcepts.length !== 0 && <h1 className="g-text font-extrabold text-3xl text-center pb-6">Explore questions to practice</h1>}
+                {filteredConcepts.length !== 0 && <h1 className="g-text font-extrabold text-3xl text-center pb-6">Explore concepts to practice</h1>}
 
                 {totalQuestions === 0 ? (
                     <>
