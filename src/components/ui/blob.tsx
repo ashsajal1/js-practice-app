a33ac7d10d357d67501daec9a67475934f74d47a
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-22 08:18:18 +0600

    style: decrease density of background

--- src/components/ui/blob.tsx
+++ src/components/ui/blob.tsx
@@ -4,7 +4,7 @@ export default function Blob({ ...props }) {
     const { className } = props
     return (
         <>
-            <div {...props} className={cn("h-[400px] w-[400px] blur-3xl bg-gradient-to-br from-blue-100 to-blue-200 rounded-full absolute -z-10 dark:hidden", className)}></div>
+            <div {...props} className={cn("h-[400px] w-[400px] blur-3xl bg-gradient-to-br from-blue-50 to-blue-100 rounded-full absolute -z-10 dark:hidden", className)}></div>
 
         </>
     )
