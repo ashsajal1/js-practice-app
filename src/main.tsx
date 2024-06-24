0e3713dca3f9eee40d7887665c2f1011f10f242f
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-24 09:10:36 +0600

    feat: provide helmet

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import './index.css'
 import { router } from './router'
 import { Provider } from "react-redux";
 import { ThemeContextProvider } from './context/theme-context'
-import store from './app/store'
+import store from './app/store';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeContextProvider>
-        <RouterProvider router={router} />
-      </ThemeContextProvider>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <ThemeContextProvider>
+          <RouterProvider router={router} />
+        </ThemeContextProvider>
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
